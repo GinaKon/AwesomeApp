7725b42ea4817f8e9d5cc6a88f19c4351191a6e9
Gina Kondyleniou <128517967+GinaKon@users.noreply.github.com>
2023-06-25 15:56:13 +0100

    New screens

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomePage from './src/screens/Home';
 import AboutUs from './src/screens/Aboutus';
+import Screen3 from './src/screens/Screen3';
+import Screen4 from './src/screens/Screen4';
 
 
 const Stack = createStackNavigator();
@@ -12,6 +14,8 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomePage} />
         <Stack.Screen name="AboutUs" component={AboutUs} />
+        <Stack.Screen name ="Screen3" component={Screen3}/>
+        <Stack.Screen name = "Screen4" component = {Screen4}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
