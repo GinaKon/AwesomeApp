b62f753e418c0025c5aa29fca861c732b4f39aed
Gina Kondyleniou <128517967+GinaKon@users.noreply.github.com>
2023-07-01 21:37:14 +0100

    modifying MyButton component for using in Login

--- src/components/MyButton.js
+++ src/components/MyButton.js
@@ -1,18 +1,33 @@
-import { View, Button, Alert} from 'react-native';
+import { useState, useEffect } from "react";
+import { View, Button, Alert } from 'react-native';
+import { useDispatch } from 'react-redux';
 
-export default function MyButton({title, color}) {
-  function randomSong() {
-    return Alert.alert('Somebody once told me the world...');
-  }
+const colorPalette = {
+  tea_rose_red: '#eac8caff',
+  pale_purple: '#f2d5f8ff',
+  pink_lavender: '#e6c0e9ff',
+  lilac: '#bfabcbff',
+  cool_gray: '#8d89a6ff',
+};
+
+export default function MyButton({ title, color }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newColor, setNewColor] = useState(color);
+
+  useEffect(() => {
+    Alert.alert('Component rendered');
+  }, []);
 
   return (
     <View>
-      <Button 
-        title={title}
-        color={color}
-        onPress={randomSong} 
+      <Button
+        title={newTitle}
+        color={newColor}
+        onPress={() => {
+          setNewTitle("Login Successful");
+          setNewColor('#eac8caff'); 
+        }}
       />
     </View>
   );
 }
-
